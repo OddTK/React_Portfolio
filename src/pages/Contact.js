6460ddfdac90b0890a2c00a1357ea4aeb7935ccf
Jesus <jesusbalandra@outlook.com>
2022-04-13 22:15:31 -0600

    created contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,7 +7,14 @@ export default function Contact() {
                 <h1>Contact Me</h1>
             </div>
             <div>
-                
+                <h3>Based in Denver, Colorado (available for remote work)</h3>
+                <br />
+                <a href="mailto:jesusbalandra@outlook.com">JESUSBALANDRA@OUTLOOK.COM</a>
+            </div>
+            <div>
+                <a href="https://www.github.com/oddtk" target="_blank">github</a>
+                <a href="https://www.linkedin.com/in/jesus-balandra" target="_">LinkedIn</a>
+                <a href="https://coursera.org/share/448e67a79f8c2a206dad422ff8b07702">Google IT Support</a>
             </div>
         </div>
     )
