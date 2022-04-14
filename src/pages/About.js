c40fea85ce36d8751585b32e83dd1d311b4eb36c
Jesus <jesusbalandra@outlook.com>
2022-04-13 22:08:17 -0600

    created repo

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function About(){
+    return(
+        <div>
+            <div>
+                <h1>About Me</h1>
+            </div>
+            <div>
+                <p>Welcome to my page! I'm Jesus! I am new new developer, studying at the University of Denver, in their Full Stack Boot Camp!</p>
+                <p>In my free time, I love learning! Astronomy has always held a special place in my heart - what can I say I love looking up at the sky and discovering new facts about space! I am also very much interested in video games! My favorite franchise being the Legend of Zelda. Growing up my parents were very much into play Super Mario. I loved watching them play and the fun we had together so it always stuck with me.</p>
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Contact() {
+    return (
+        <div>
+            <div>
+                <h1>Contact Me</h1>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
