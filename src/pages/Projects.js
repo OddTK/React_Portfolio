55cf9c561459783716194ee8ff2ad6fa2bc9cebd
Jesus <jesusbalandra@outlook.com>
2022-04-27 08:29:25 -0600

    added img directory with files

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,7 +4,9 @@ import '../style/Style.css';
 export default function Projects() {
     return (
         <section>
-            <h1 className="h1">Projects</h1>
+            <div>
+                <h1 className="h1">Projects</h1>
+            </div>
         </section>
     )
 }
